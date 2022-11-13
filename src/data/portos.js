97044f5faaaf87d01f94d3f8d16e3e86eecbeee4
IlyasBudi <wjati145@gmail.com>
2022-11-13 20:50:11 +0700

    TPA-4-React-Personal-Website

--- src/data/portos.js
+++ src/data/portos.js
@@ -2,25 +2,25 @@ export default [
 	{
 		title: 'Personal Website Using HTML, CSS',
 		img:
-			'../images/personal-website.png',
+			'/src/images/personal-website.png',
 		url: 'https://ilyas-budii.netlify.app',
 	},
 	{
 		title: "Calculator BMI",
 		img:
-			'../images/calculator-bmi.png',
+			'/src/images/calculator-bmi.png',
 		url: 'https://ilyasbudi-bmicalculator.netlify.app',
 	},
 	{
 		title: "Movie App with TMDB API",
 		img:
-			'../images/Movie-app.png',
+			'/src/images/Movie-app.png',
 		url: 'https://ilyasbudi-movieapp.netlify.app',
 	},
 	{
 		title: 'Personal Website Using React, Tailwind',
 		img:
-			'../images/react-personal-website.png',
+			'/src/images/react-personal-website.png',
 		url: 'https://ilyas-budi.netlify.app',
 	},
 ];
