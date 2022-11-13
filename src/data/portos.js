6557cd26f8d37e5e463eb7828dd929d044d2f35c
IlyasBudi <wjati145@gmail.com>
2022-11-13 20:39:47 +0700

    TPA-4-React-Personal-Website

--- src/data/portos.js
+++ src/data/portos.js
@@ -2,25 +2,25 @@ export default [
 	{
 		title: 'Personal Website Using HTML, CSS',
 		img:
-			'/src/images/personal-website.png',
+			'../images/personal-website.png',
 		url: 'https://ilyas-budii.netlify.app',
 	},
 	{
 		title: "Calculator BMI",
 		img:
-			'/src/images/calculator-bmi.png',
-		url: 'https://github.com/ShaifArfan/shaif-s-cuisine',
+			'../images/calculator-bmi.png',
+		url: 'https://ilyasbudi-bmicalculator.netlify.app',
 	},
 	{
 		title: "Movie App with TMDB API",
 		img:
-			'/src/images/Movie-app.png',
+			'../images/Movie-app.png',
 		url: 'https://ilyasbudi-movieapp.netlify.app',
 	},
 	{
 		title: 'Personal Website Using React, Tailwind',
 		img:
-			'/src/images/react-personal-website.png',
+			'../images/react-personal-website.png',
 		url: 'https://ilyas-budi.netlify.app',
 	},
 ];
